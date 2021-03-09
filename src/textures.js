b822e6f902844c10119dfa0ed48662676442cd04
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-03-09 17:27:00 +0100

    il est cool git

--- src/textures.js
+++ src/textures.js
@@ -1,5 +1,21 @@
 'use strict';
 
+function solid(i, j)
+{
+    return [0, 0, 0, 255]; 
+}
+
+function gradient_Horizontal(i, j)
+{
+    return [Math.floor(255*j/height), Math.floor(255*j/height), Math.floor(255*j/height), 255];
+}
+
+function gradient_Vertical(i, j)
+{
+    return [Math.floor(255*i/width), Math.floor(255*i/width), Math.floor(255*i/width), 255];
+}
+
+
 function regulatePavage_square(i, j, nbSquare_x, nbSquare_y)
 {
     let sizeSquare_x = width/nbSquare_x; 
