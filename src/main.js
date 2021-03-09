d677cd2f11a0c77837d97735376a24068813c8be
Léna Herau <perig.herau@enseirb-matmeca.fr>
2021-03-09 17:46:27 +0100

    Main generateImage function

--- src/main.js
+++ src/main.js
@@ -16,29 +16,55 @@ const couleurs = {
     white: [255, 255, 255, 255],     
 }
 
+//////// MAIN FUNCTIONS ///////////
+
 // Uses texture function with arguments args to print on canvas
 function generateImage(canvas, texture, ...args) {
     // Environnment definition
     let context = canvas.getContext("2d");
     let image = context.createImageData(canvas.width, canvas.height);
     
-    let map = [], column = [];
+    // Texture function definition
     let nbArgs = 0;
     let textureFunction = texture;
     while(typeof(textureFunction) === 'function' && nbArgs <= MAX_ARGUMENTS){
+	// Verifies that next argument is not the last block
 	if(typeof(textureFunction(args[0])) === 'function'){
 	    textureFunction = textureFunction(args[0]);
 	}
-	console.log(typeof(textureFunction));
-	params.shift();
-	count++;
+	args.shift();
+	nbArgs++;
     }
-    for (let n = 0, x = 0; x < width; x++) {
+
+    // Texture application
+    let column = [];
+    for (let n = 0, x = 0; x < canvas.width; x++) {
 	column = [];
-	for (let y = 0; y < height; y++) {
+	for (let y = 0; y < canvas.height; y++, n += 4) {
 	    let pixel = textureFunction(x,y);
+	    image.data[n]   = pixel[0]; // Red channel
+	    image.data[n+1] = pixel[1]; // Green channel
+	    image.data[n+2] = pixel[2]; // Blue channel
+	    image.data[n+3] = pixel[3]; // Alpha channel
 	}
-	map.push(column);
     }
-    return map;
+    
+    // Image printing
+    context.putImageData(image, 0, 0);
 }
+
+
+///////// TESTS (temporary) //////////
+
+// Texture function for horizontal bi-color gradient (does stuff with color)
+function textureHorizontalGradient(width) {
+    return function (color) {
+	return function (x,y) {
+	    return [Math.floor(255*color/width), 
+		    Math.floor(255*x/width), 
+		    Math.floor(255*x/width), 255];
+	};
+    };
+}
+
+generateImage(CANVAS, textureHorizontalGradient, canvas.width, 128);
