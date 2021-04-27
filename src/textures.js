b88d633d0969f725a635f34728863240962db396
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-04-27 14:40:19 +0200

    correct perlinNoise with dict

--- src/textures.js
+++ src/textures.js
@@ -1067,92 +1067,96 @@ function texture_squareTiling(dict) {
  * @param (i,j) coordinates of the pixel
  * @return a colored pixel corresponding to (x,y) position   
  */
-function texture_perlinNoise(row) {
+function texture_perlinNoise() {
+    const rows = dict['rows']       || 5
+    const colums = dict['columns']  || 5
+    const colors = dict['colors']   || [COLORS.blue, COLORS.green, COLORS.red]
+
+    /*
+    row
     return function (column) {
         return function (nb_colors) {
             return function (colors) {
+    */
 
-                let stock_gradient = {};
+    let stock_gradient = {};
 
-                // =====================================================
+    // =====================================================
 
-                // Fade interpolation 
-                function interpolate(smoothstep) {
-                    return (v0, v1, t) => (v1 - v0) * smoothstep(t) + v0;
-                }
+    // Fade interpolation 
+    function interpolate(smoothstep) {
+        return (v0, v1, t) => (v1 - v0) * smoothstep(t) + v0;
+    }
 
-                const fade_Interpolation = interpolate(t => t * t * t * (10 - 15 * t + 6 * t * t));
+    const fade_Interpolation = interpolate(t => t * t * t * (10 - 15 * t + 6 * t * t));
 
-                /*
-                *   Create random direction vector 
-                *   Used for the Grid 
-                */
-                function random_Vector() {
-                    const random = Math.random() * 2 * Math.PI;
-                    return [Math.cos(random), Math.sin(random)];
-                }
+    /*
+    *   Create random direction vector 
+    *   Used for the Grid 
+    */
+    function random_Vector() {
+        const random = Math.random() * 2 * Math.PI;
+        return [Math.cos(random), Math.sin(random)];
+    }
 
-                /*
-                * Computes the dot product of the distance and gradient vectors
-                */
-                function dot_Product(vector, distance) {
-                    return (distance[0] * vector[0] + distance[1] * vector[1]);
-                }
+    /*
+    * Computes the dot product of the distance and gradient vectors
+    */
+    function dot_Product(vector, distance) {
+        return (distance[0] * vector[0] + distance[1] * vector[1]);
+    }
 
-                function get_Dot(ix, iy, x_grid, y_grid) {
-                    // Get a vector
-                    if (!stock_gradient[[ix, iy]])
-                        stock_gradient[[ix, iy]] = random_Vector(ix, iy);
+    function get_Dot(ix, iy, x_grid, y_grid) {
+        // Get a vector
+        if (!stock_gradient[[ix, iy]])
+            stock_gradient[[ix, iy]] = random_Vector(ix, iy);
 
-                    // Calculate distance 
-                    const dx = x_grid - ix;
-                    const dy = y_grid - iy;
+        // Calculate distance 
+        const dx = x_grid - ix;
+        const dy = y_grid - iy;
 
-                    return dot_Product(stock_gradient[[ix, iy]], [dx, dy]);
-                }
+        return dot_Product(stock_gradient[[ix, iy]], [dx, dy]);
+    }
 
-                // Return a color depending of the value and the colors permited 
-                function get_Colors(nb_colors, value) {
-                    return Math.floor(value / ([1, 2, 3, 4, 5].map((x) => 1 / x))[nb_colors - 1]);
-                }
+    // Return a color depending of the value and the colors permited 
+    function get_Colors(value) {
+        return Math.floor(value / ([1, 2, 3, 4, 5].map((x) => 1 / x))[colors.length - 1]);
+    }
 
-                // =====================================================
+    // =====================================================
 
-                /*
-                * Compute Perlin noise at coordinates x,y 
-                */
-                return (x, y) => {
-                    // Determine precise coordinates in the grid
-                    const x_grid = x / row;
-                    const y_grid = y / column;
+    /*
+    * Compute Perlin noise at coordinates x,y 
+    */
+    return (x, y) => {
+        // Determine precise coordinates in the grid
+        const x_grid = x / row;
+        const y_grid = y / column;
 
-                    // Determine grid cell coordinates
-                    const x0 = Math.floor(x_grid);
-                    const x1 = x0 + 1;
-                    const y0 = Math.floor(y_grid);
-                    const y1 = y0 + 1;
+        // Determine grid cell coordinates
+        const x0 = Math.floor(x_grid);
+        const x1 = x0 + 1;
+        const y0 = Math.floor(y_grid);
+        const y1 = y0 + 1;
 
-                    // Determine interpolation weights
-                    const distance = [x_grid - x0, y_grid - y0];
+        // Determine interpolation weights
+        const distance = [x_grid - x0, y_grid - y0];
 
-                    // Interpolate between grid point gradients
-                    const Up_Left = get_Dot(x0, y0, x_grid, y_grid);
-                    const Up_Right = get_Dot(x1, y0, x_grid, y_grid);
-                    const Down_Left = get_Dot(x0, y1, x_grid, y_grid);
-                    const Down_Right = get_Dot(x1, y1, x_grid, y_grid);
+        // Interpolate between grid point gradients
+        const Up_Left = get_Dot(x0, y0, x_grid, y_grid);
+        const Up_Right = get_Dot(x1, y0, x_grid, y_grid);
+        const Down_Left = get_Dot(x0, y1, x_grid, y_grid);
+        const Down_Right = get_Dot(x1, y1, x_grid, y_grid);
 
-                    let value = fade_Interpolation(
-                        fade_Interpolation(Up_Left, Up_Right, distance[0]),
-                        fade_Interpolation(Down_Left, Down_Right, distance[0]),
-                        distance[1]
-                    );
+        let value = fade_Interpolation(
+            fade_Interpolation(Up_Left, Up_Right, distance[0]),
+            fade_Interpolation(Down_Left, Down_Right, distance[0]),
+            distance[1]
+        );
 
-                    // Making value between 0 and 1
-                    value = (value + 1) / 2;
-                    return colors[get_Colors(nb_colors, value)];
-                };
-            };
-        };
+        // Making value between 0 and 1
+        value = (value + 1) / 2;
+        return colors[get_Colors(value)];
     };
 }
 
